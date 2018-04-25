228425d7ea0b3063d7b6578955874e1d27a19b73
Teryaev Sergey <Stormcoder74@users.noreply.github.com>
2018-04-26 00:13:47 +1100

    3 и 4 задачи дз

---
+++ lesson_01/homework/task_4_main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+long const X = 7;
+long const N = 15;
+
+long quickPow(long x, long n) {
+    if (n > 3) {
+        long tmpX = quickPow(x, n / 2);
+        if (n % 2 == 0) {
+            x = tmpX * tmpX;
+        } else {
+            x = x * tmpX * tmpX;
+        }
+    } else {
+        if (n % 2 == 0) {
+            x = x * x;
+        } else {
+            x = x * x * x;
+        }
+    }
+    return x;
+}
+
+int main() {
+    printf("Число %ld в степени %ld равно %ld\n", X, N, quickPow(X, N));
+    return 0;
+}
---
+++ lesson_01/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <time.h>
+
+int const N = 500000; // не пойму почему при бОльших значениях программа совсем не работает и ошибок не дает
+
+int main() {
+    int arr[N];
+    for (int i = 2; i < N; i++) {
+        arr[i] = 1;
+    }
+
+    int time1 = (int) clock();
+
+    for (int i = 2; i * i < N; i++) {
+        if (arr[i] == 1) {
+            for (int j = i * i; j < N; j = (i == 2) ? (j + i) : (j + 2 * i)) {
+                if (arr[j] == 1) {
+                    arr[j] = 0;
+                    //printf("i = %d, j = %d\n", i, j);
+                }
+            }
+        }
+    }
+
+    for (int i = N - 1; i > 2; i--) {
+        if (arr[i]) {
+            printf("Максимальное простое число в диапазоне от 2 до %d: %d\n", N, i);
+            printf("Вычисления заняли %d милисекунд\n", (int) (clock() - time1));
+            break;
+        }
+    }
+    /*for (int i = 2; i < N; i++) {
+        if (arr[i]) printf("%5d", i);
+        if (i % 100 == 0) printf("\n");
+    }*/
+    return 0;
+}
+

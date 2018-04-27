e54c86743b0e16def881efa65ef90ce4acb7e476
Teryaev Sergey <Stormcoder74@users.noreply.github.com>
2018-04-27 22:40:01 +1100

    задача 3 рождается в муках.

--- lesson_02/main.c
+++ lesson_02/main.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
+#include <math.h>
 
-int const NUMBER = 345; // 101011001
-int const NOTATION_BASE = 2;
+int const GOAL = 20;
+int const START = 3;
 
-int toBin(int x) {
-    if (x / NOTATION_BASE) return toBin(x / NOTATION_BASE) * 10 + x % NOTATION_BASE;
-    else return x % NOTATION_BASE;
+void toBin(int x, int *start, int goal) {
+    if (x / 2) {
+        toBin(x / 2, start, goal);
+    }
+    printf("%d", x % 2);
+    if (x % 2) {
+        *start *= 2;
+    } else {
+        *start += 1;
+    }
 }
 
 int main() {
-    printf("%d\n", toBin(NUMBER));
+    for (int c = 1; c < 5; c++) {
+        int signs = pow(2, c);
+        for (int i = 0; i < signs; i++) {
+            int start = START;
+            toBin(i, &start, GOAL);
+            printf(" %d\n", start);
+        }
+        printf("\n");
+    }
     return 0;
 }

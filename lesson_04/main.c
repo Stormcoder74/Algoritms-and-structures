88f3b12439d7f723cc276d45c8d8bb1e24c2f81f
Teryaev Sergey <Stormcoder74@users.noreply.github.com>
2018-05-06 23:36:51 +1100

    задача о ходе коня решена с использованием правила Варндорфа

--- lesson_04/main.c
+++ lesson_04/main.c
@@ -1,57 +1,26 @@
+//https://github.com/Stormcoder74/Algoritms-and-structures.git
 #include <stdio.h>
-#include <time.h>
 
-#define SIZE 8 // 8
-int moves[8][2] = {{1,  -2},
-                   {2,  -1},
-                   {2,  1},
-                   {1,  2},
-                   {-1, 2},
-                   {-2, 1},
-                   {-2, -1},
-                   {-1, -2}};
-int board[SIZE][SIZE] = {{0},
-                         {0},
-                         {0},
-                         {0},
-                         {0},
-                         {0},
-                         {0},
-                         {0}};
+#define SIZE 8
+#define START_X 2
+#define START_Y 3
 
-int checkBoard() {
-    for (int i = 0; i < SIZE; i++)
-        for (int j = 0; j < SIZE; j++)
-            if (board[i][j] == 0)
-                return 0;
-    return 1;
-}
+int moves[8][2] = {{1,  -2}, {2,  -1}, {2,  1}, {1,  2},
+                   {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}};
+int board[SIZE][SIZE] = {{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}};
 
 void printBoard() {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++)
-            printf("%d\t", board[i][j]);
+            printf("%3d", board[i][j]);
         printf("\n");
     }
     printf("\n");
 }
 
-void invert(int start, int finish) {
-    for (int i = 0; i <= (finish - start) / 2; i++) {
-        for (int j = 0; j < SIZE; ++j) {
-            for (int k = 0; k < SIZE; ++k) {
-                if (board[j][k] == start + i) {
-                    board[j][k] = finish - i;
-                    continue;
-                }
-                if (board[j][k] == finish - i)
-                    board[j][k] = start + i;
-            }
-        }
-    }
-}
-
-void stepSelect(int *x, int *y) {
+// Правило Варнсдорфа: При обходе доски конь следует на то поле,
+// с которого можно пойти на минимальное число ещё не пройденных полей.
+void ruleOfVarnsdorf(int *x, int *y) {
     int min = 8;
     int numMove = 0;
     for (int i = 0; i < 8; ++i) {
@@ -60,15 +29,13 @@ void stepSelect(int *x, int *y) {
         if ((X >= 0 && X < SIZE) && (Y >= 0 && Y < SIZE)) {
             if (board[Y][X] == 0) {
                 int counter = 0;
-                int X2, Y2;
                 for (int j = 0; j < 8; ++j) {
-                    X2 = X + moves[j][0];
-                    Y2 = Y + moves[j][1];
-                    if ((X2 >= 0 && X2 < SIZE) && (Y2 >= 0 && Y2 < SIZE) ) { //&& (X2 != *x && Y2 != *y)
+                    int X2 = X + moves[j][0];
+                    int Y2 = Y + moves[j][1];
+                    if ((X2 >= 0 && X2 < SIZE) && (Y2 >= 0 && Y2 < SIZE))
                         if (board[Y2][X2] == 0) {
                             counter++;
                         }
-                    }
                 }
                 if (counter < min) {
                     min = counter;
@@ -81,50 +48,12 @@ void stepSelect(int *x, int *y) {
     *y = *y + moves[numMove][1];
 }
 
-int findMove(int x, int y, int step) {
-    for (int j = 0; j < 8; j++) {
-        int X = x, Y = y;
-        stepSelect(&X, &Y);
-        if ((X >= 0 && X < SIZE) && (Y >= 0 && Y < SIZE)) {
-            if (board[Y][X] == 0) {
-                board[Y][X] = step;
-                if (step == SIZE * SIZE)
-                    return step;
-                else
-                    return findMove(X, Y, step + 1);
-            }
-        }
-    }
-    return step - 1;
-}
-
 int main(void) {
-//    long time1 = (int) clock();
-    int result = findMove(1, 1, 1);
-    printBoard();
-    /*invert(1, result);
-    for (int j = 0; j < SIZE; ++j) {
-        for (int k = 0; k < SIZE; ++k) {
-            if (board[j][k] == result) {
-                result = findMove(k, j, result + 1);
-            }
-        }
+    int x = START_X, y = START_Y;
+    for (int i = 1; i <= SIZE * SIZE; ++i) {
+        board[y][x] = i;
+        ruleOfVarnsdorf(&x, &y);
     }
     printBoard();
-    invert(40, result);
-    for (int j = 0; j < SIZE; ++j) {
-        for (int k = 0; k < SIZE; ++k) {
-            if (board[j][k] == result) {
-                result = findMove(k, j, result + 1);
-            }
-        }
-    }
-    printBoard();
-    invert(51, result);
-    printBoard();*/
-
-
-//    long time2 = (int) clock();
-//    printf("%lf", (double)(time2 - time1) / 1000);
     return 0;
 }

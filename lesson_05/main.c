a03ef7866a3ae1de350cc82d5d31ecd59102d083
Teryaev Sergey <Stormcoder74@users.noreply.github.com>
2018-05-10 23:54:32 +1100

    в процессе решения 4 задачи

--- lesson_05/main.c
+++ lesson_05/main.c
@@ -1,105 +1,92 @@
+//https://github.com/Stormcoder74/Algoritms-and-structures.git
 #include <stdio.h>
+#include <string.h>
 #include <malloc.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 #define T char
-// Опишем структуру узла списка
+
 struct TNode {
-    T value;
-    struct TNode *next;
-    struct TNode *prev;
+    T value; // Данные
+    struct TNode *next; // Указатель на следующий элемент списка
 };
 typedef struct TNode Node;
+Node *head = NULL;
 
-struct Deck {
-    Node *head;
-    Node *tail;
-    int size;
-    int maxSize;
-};
-struct Deck deck;
-
-enum Direction {
-    HEAD_TAIL, TAIL_HEAD
-};
-typedef enum Direction Direction;
-
-void push(T value, Direction dir) {
-    if (deck.size >= deck.maxSize) {
-        printf("Error deck size");
-        return;
-    }
-    Node *tmp = (Node *) malloc(sizeof(Node));
-    tmp->value = value;
-    if (deck.size == 0) {
-        tmp->next = NULL;
-        tmp->prev = NULL;
-        deck.head = tmp;
-        deck.tail = tmp;
-    } else if (dir == HEAD_TAIL) {
-        tmp->next = deck.head;
-        tmp->next->prev = tmp;
-        deck.head = tmp;
-    } else {
-        tmp->prev = deck.tail;
-        tmp->prev->next = tmp;
-        deck.tail = tmp;
-    }
-    deck.size++;
+void Push(T value) {
+    Node *temp;
+    temp = (Node *) malloc(sizeof(Node));
+    temp->value = value;
+    temp->next = head;
+    head = temp;
 }
 
-T pop(Direction dir) {
-    if (deck.size == 0) {
-        printf("deck is empty");
-        return NULL;
-    }
-// Временный указатель
-    Node *tmp = NULL;
-// Значение "наверху" списка
-    T value;
-    if (deck.size == 1) {
-        value = deck.head->value;
-        tmp = deck.head;
-        deck.head = NULL;
-        deck.tail = NULL;
-    } else if (dir == HEAD_TAIL) {
-        value = deck.tail->value;
-        tmp = deck.tail;
-        deck.tail = deck.tail->prev;
-        deck.tail->next = NULL;
-    } else {
-        value = deck.head->value;
-        tmp = deck.head;
-        deck.head = deck.head->next;
-        deck.head->prev = NULL;
-    }
-
-
-// Запись, на которую указывала голова удаляем, освобождая память
-    free(tmp);
-// Возвращаем значение, которое было в голове
-    deck.size--;
+T Pop() {
+    Node *temp = NULL;
+    T value = head->value;
+    temp = head;
+    head = head->next;
+    free(temp);
     return value;
 }
 
-void PrintStack() {
-    Node *current = deck.head;
-    while (current != NULL) {
-        printf("%c ", current->value);
-        current = current->next;
+void toOutput(char *output, char *el, int *index) {
+    int i = 0;
+    while (el[i] != '\0') {
+        output[*index] = el[i];
+        i++;
+        *index += 1;
     }
+    output[*index] = ' ';
+    *index += 1;
+}
+
+int isNumber(char *str) {
+    int i = 0;
+    while (str[i] != '\0')
+        if (!isdigit(str[i++]))
+            return 0;
+    return 1;
 }
 
 int main(int argc, char *argv[]) {
-    deck.maxSize = 100;
-    deck.head = NULL;
-    deck.tail = NULL;
-    push('a', HEAD_TAIL);
-    push('b', TAIL_HEAD);
-    push('c', HEAD_TAIL);
-    push('d', TAIL_HEAD);
-    push('e', HEAD_TAIL);
-    push('f', TAIL_HEAD);
-    PrintStack();
-    printf("\n");
+    char input[100] = "3 + 23 * 4 - ( 45 + 32 ) / 6"; // входная строка
+    char output[100] = ""; // выходная строка
+    int outIndex = 0;
+
+    for (int i = 0; i < strlen(input); i++) {
+        char el[20]; // элемент(число или операция)
+        int j = 0;
+        while (input[i] != ' ' && input[i] != '\0') {
+            el[j] = input[i];
+            j++;
+            i++;
+        }
+        el[j] = '\0';
+// Если элемент Число
+        if (isNumber(el))
+            toOutput(output, el, &outIndex);
+        else {
+            switch (el[0]) {
+                case '+':
+
+                    break;
+                case '-':
+
+                    break;
+                case '*':
+
+                    break;
+                case '/':
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    printf("%d", Pop());
+    getchar();
     return 0;
 }
